0115d18be568aec674693b7fa22faeb9c8389dc9
aceyboyce <aceyboyce@a0cce2b4-aaee-11dd-9f94-e5dfbee8a4f1>
2008-11-23 20:35:33 +0000

    Back end for all unit movement and attacking.  Only missing undo.

---
+++ trunk/Tanktics/NullUnit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tanktics
+{
+    class NullUnit: Unit
+    {
+        public NullUnit()
+        {
+            type = "null";
+            team = 0;
+        }
+    }
+}
