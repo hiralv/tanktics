83b52a01c0ee0baea88c9784ab79ecc7bbc19785
rhflorence <rhflorence@a0cce2b4-aaee-11dd-9f94-e5dfbee8a4f1>
2008-12-14 07:08:28 +0000

    commented out fullscreen for now so we can see error messages need some of this stuff for new unit explosion content

--- Tanktics/Game.cs
+++ Tanktics/Game.cs
@@ -93,7 +93,7 @@ namespace Tanktics
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
-            this.graphics.IsFullScreen = true;
+            //this.graphics.IsFullScreen = true;
 
             // Create the screen manager component.
             screenManager = new ScreenManager(this);
